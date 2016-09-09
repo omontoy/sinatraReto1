948acabfe36ee69b3726f0c0c8b837cdf1c6648f
Orlando Montoya <omb0320@gmail.com>
2016-09-09 12:04:23 -0500

    Reto 1 terminado

--- solution.rb
+++ solution.rb
@@ -2,17 +2,9 @@ require 'sinatra'
 
 get '/' do 
 	unless params[:name]
-		<h1>"Hola desconocido!"</h1>
+		"Hola desconocido!"
 	else
-		<h1>"Hola #{params[:name]}"</h1>
+		"Hola #{params[:name]}"
 	end
 end
 
-=begin
-get '/:name' do
-  "Hola #{params[:name]}"
-end
-
- "/?name=orlando"
-params = {name: "orlando"}
-=end
